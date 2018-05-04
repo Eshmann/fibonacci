b33d1fdb3a116a9964a584374254a4b3e6b1ad6f
Oleh <radyukolegg@gmail.com>
2018-05-04 14:11:25 +0000

    inter

--- js/index.js
+++ js/index.js
@@ -1,18 +1,34 @@
-a = prompt(" Enter the number: ");
-fib1 = 0;
-fib2 = 1;
-
+let a = +prompt(" Enter the number: ");
+let fib1 = 1;
+let fib2 = 1;
+let fib3;
+function fibonacci(a){
+    
 document.write("<center><h1>"+'Fibonacci numbers <br>');
-document.write("******************************<br>")
-document.write(fib1 + '<br>');
-document.write(fib2 + '<br>');
+document.write("******************************<br>");
+// document.write(fib1 + '<br>');
+// document.write(fib2 + '<br>');
 
-for(i=2; i<=a; i++)
-{
+for(let i=3; i<=a; i++){
+
     fib3 = fib1 + fib2;
-    document.write(fib3)
-    fib1 = fib2
-    fib2 = fib3
-    document.write('<br>');
+    fib1 = fib2;
+    fib2 = fib3;
+    // document.write('<br>');
 }
+return fib2;
+
+}
+document.write(fibonacci(a) + "<br>");
 document.write("******************************<br></h1></center>");
+
+function fibonacci2(a){
+
+    if(a <= 1){
+      return a;
+    } 
+    else {
+      return fibonacci2(a - 1) + fibonacci2(a - 2);        
+    }
+}
+document.write(`recurcia method : ` + fibonacci2(a));
